caf769cc9fff7ec609222395bb35ef2c489806a0
NikethaDinesh <56196202+NikethaDinesh@users.noreply.github.com>
2020-11-24 10:50:50 +0530

    Add files via upload

---
+++ Day 2/variable.py
@@ -0,0 +1,14 @@
+a=b=c=50
+print(a/10)
+print(b*50)
+print(c+60)
+
+str ="stack"
+index = 2        #replaces third character
+replace_char="G"
+print(str[:index]+replace_char+str[index+1:])
+
+a=5
+b=3.14
+print(float(a))    #int to float
+print(int(b))      #float to int
