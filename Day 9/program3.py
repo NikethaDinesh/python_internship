30523c701af39c26c2cd40ed45a4caa9d080f86a
Niketha <56196202+NikethaDinesh@users.noreply.github.com>
2020-12-02 12:21:57 +0530

    Add files via upload

---
+++ Day 9/program3.py
@@ -0,0 +1,4 @@
+n=int("enter no.:"input())
+lst=[12,10,8,6]
+lst1=list(map(lambda x:x*n,lst))
+print(lst1)
---
+++ Day 9/program4.py
@@ -0,0 +1,3 @@
+lst=[4,9,18,45,50,36]
+lst2=list(filter(lambda x:x%9==0,lst))
+print("filtered list:",lst2)
